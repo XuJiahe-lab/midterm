9bc362210d383ea8d71da6ca42e292e1b22e58ff
XuJiahe-lab <56863155+XuJiahe-lab@users.noreply.github.com>
2019-11-25 22:20:07 +0800

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -2,12 +2,13 @@
 int bitcount(unsigned long long int x);
 int main (void)
 {
-    unsigned long long int m, n;
+    unsigned long long int m, n, p;
     m = 0;
     n = ~m;
+    p = n + 1;
     printf(" unsigned long long int is size of %d bit .",bitcount(1));
     printf("The max is %llu\n", n);
-    printf(", the min is");
+    printf(", the min is %llu", p);
     return 0;
 }
     int bitcount (unsigned long long int x)
